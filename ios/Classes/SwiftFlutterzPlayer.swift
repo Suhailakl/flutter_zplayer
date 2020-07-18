2dfc78f80834f64f3ba6883b5a928396024b5b00
Suhailakl <suhail0404@gmail.com>
2020-07-18 21:36:18 +0530

    iOS plugin issue solved

--- ios/Classes/SwiftFlutterzPlayer.swift
+++ ios/Classes/SwiftFlutterzPlayer.swift
@@ -6,7 +6,7 @@ public class SwiftFlutterzPlayer: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     
     /* register audio player */
-    AudioPlayer.register(with: registrar)
+//    AudioPlayer.register(with: registrar)
     
     /* register video player */
     VideoPlayerFactory.register(with: registrar)
