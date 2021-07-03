c957e212573fd4aa63e9ecda5cf3964f834b171d
Suhailakl <suhail0404@gmail.com>
2021-07-03 16:53:45 +0530

    Update SwiftFlutterzPlayer.swift

--- ios/Classes/SwiftFlutterzPlayer.swift
+++ ios/Classes/SwiftFlutterzPlayer.swift
@@ -4,11 +4,6 @@ import UIKit
 public class SwiftFlutterzPlayer: NSObject, FlutterPlugin {
     
   public static func register(with registrar: FlutterPluginRegistrar) {
-    
-    /* register audio player */
-//    AudioPlayer.register(with: registrar)
-    
-    /* register video player */
-    VideoPlayerFactory.register(with: registrar)
+
   }
 }
